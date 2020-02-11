ddeaefd29e28e8f3a7c096e70684d7d6415a1534
@Vladyslav_Soloviov <Vladyslav_Soloviov@epam.com>
2020-02-12 01:07:46 +0200

    created simple express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const logger = require('./middleware/logger');
+const homepage = require('./routes/homepage');
+const express = require('express');
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+
+// routes
+app.use('/', homepage);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
